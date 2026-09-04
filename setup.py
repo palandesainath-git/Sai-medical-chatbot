654b839958721f8bb0ecb8b3ef87692c9a347613
palandesainath-git <palandesainath@gmail.com>
2026-09-05 02:09:17 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import find_packages,setup
+
+setup(
+    name="Sai-medical-chatbot",
+    version="0.1.1",
+    author="sainath palande", 
+    author_email="palandesainath@gmail.com", 
+    packages=find_packages(),
+    install_requires=[]
+)
